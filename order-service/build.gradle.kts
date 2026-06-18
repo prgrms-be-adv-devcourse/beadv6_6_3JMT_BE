44c0ecfdb077444df55f69bbe88fdcb1fdeafb2c
Chan <ieejo716@naver.com>
2026-06-18 11:47:17 +0900

    chore: build.gradle.kts 의존성 추가 (validation, lombok)

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -7,8 +7,10 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("org.postgresql:postgresql")
