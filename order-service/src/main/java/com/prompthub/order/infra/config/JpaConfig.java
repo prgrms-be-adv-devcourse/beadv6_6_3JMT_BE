944d20fa73a6274b7338535d598e473cd8104b28
Chan <ieejo716@naver.com>
2026-06-22 12:19:57 +0900

    chore: JPA & Auditing 설정 분리 및 테스트 설정 추가

---
+++ order-service/src/main/java/com/prompthub/order/infra/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.prompthub.order.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "auditingAware")
+public class JpaConfig {
+
+    @Bean
+    public DateTimeProvider auditingAware() {
+        return () -> Optional.of(LocalDateTime.now());
+    }
+}
