36b7f5bf16b20d98c170733ade66236e4b382c90
Chan <ieejo716@naver.com>
2026-06-27 03:58:05 +0900

    refactor: 사용자 권한 상수명 변경 및 테스트 코드 반영
    - 인증 헤더에 사용되는 일반 사용자 권한 상수를 USER에서 BUYER로 교체
    - 주문 서비스 내 인증 인터셉터 권한 검사 로직에 BUYER 권한 적용
    - 관리자 컨트롤러 및 장바구니 등 모든 API 테스트 코드에 변경된 권한 일괄 반영

--- order-service/src/main/java/com/prompthub/order/global/web/AuthHeaders.java
+++ order-service/src/main/java/com/prompthub/order/global/web/AuthHeaders.java
@@ -4,7 +4,7 @@ public final class AuthHeaders {
 
 	public static final String USER_ID = "X-User-Id";
 	public static final String USER_ROLE = "X-User-Role";
-	public static final String USER = "USER";
+	public static final String BUYER = "BUYER";
 	public static final String SELLER = "SELLER";
 	public static final String ADMIN = "ADMIN";
 
