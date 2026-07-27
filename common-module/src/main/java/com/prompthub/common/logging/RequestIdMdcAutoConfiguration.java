f709780fa2790daaa7c2214724ede5248c3dbfbf
Chan <ieejo716@naver.com>
2026-07-28 01:18:14 +0900

    feat: common-module HTTP 요청 ID MDC 추적 추가

---
+++ common-module/src/main/java/com/prompthub/common/logging/RequestIdMdcAutoConfiguration.java
@@ -0,0 +1,22 @@
+package com.prompthub.common.logging;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+
+@AutoConfiguration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class RequestIdMdcAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(name = "requestIdMdcFilterRegistration")
+    FilterRegistrationBean<RequestIdMdcFilter> requestIdMdcFilterRegistration() {
+        FilterRegistrationBean<RequestIdMdcFilter> registration =
+                new FilterRegistrationBean<>(new RequestIdMdcFilter());
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registration;
+    }
+}
